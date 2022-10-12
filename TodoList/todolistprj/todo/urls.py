734d72bdfb96c243a3da213c0d933381f446bc28
Sarayu-sowdala17 <115654017+Sarayu-sowdala17@users.noreply.github.com>
2022-10-12 22:18:36 +0530

    Add files via upload

---
+++ TodoList/todolistprj/todo/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, UserLogin, Register
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('', TaskList.as_view(), name='task-list'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
+    path('login/', UserLogin.as_view(), name='user-login'),
+    path('register/', Register.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(next_page='user-login'), name='user-logout'),
+]
